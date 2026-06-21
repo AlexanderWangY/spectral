a93cefed2cf6794ebdef064d5b4bae7f876866a7
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-06-21 16:21:26 -0700

    feat: runtime skeleton

---
+++ src/runtime.rs
@@ -0,0 +1,15 @@
+pub struct Runtime {}
+
+impl Runtime {
+    pub fn start() -> Self {
+        // Initialize mspc channels
+
+        // Construct actors
+
+        // Inject channels
+
+        // Kick off run
+
+        Self {}
+    }
+}
